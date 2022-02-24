5d968a755ea40371b80e8e4fb5b7d78b8307c859
Thor Stenbæk <tst@dips.no>
2022-02-24 19:13:49 +0100

    Added touch

--- src/lib/transformation.ts
+++ src/lib/transformation.ts
@@ -26,19 +26,31 @@ export default function transformation(element: HTMLElement): ReturnType<Action>
         onDown(event, event.clientX, event.clientY);
     }
 
-    function onDown(event: Event, x: number, y: number) {
-        window.addEventListener("mouseup", onMouseUp);   
+    function onDown(event: Event, x: number, y: number) {        
+        window.addEventListener("mousemove", onMouseMove);
+        element.addEventListener("touchmove", onTouchMove);
+        window.addEventListener("mouseup", onUp);   
+        element.addEventListener("touchend", onUp);
         _initialPoint = new DOMPoint(x, y);                
         event.preventDefault();
     }
 
+    function onTouchMove(event: TouchEvent) {
+        if (event.touches?.length == 1) {
+            onMove(event, event.touches[0].pageX, event.touches[0].pageY);
+        }
+    }
+
     function onMouseMove(event: MouseEvent) {
-        
+        onMove(event, event.clientX, event.clientY);
+    }
+
+    function onMove(event: Event, x: number, y: number) {
         if (_initialPoint) {            
             _current = _transformation;
 
             _current = _current.multiply(new DOMMatrixReadOnly().translate(
-                event.clientX - _initialPoint.x, event.clientY - _initialPoint.y));
+                x - _initialPoint.x, y - _initialPoint.y));
             //_transformation = _transformation.multiply(new DOMMatrixReadOnly().rotate(0.1));
             //_transformation = _transformation.multiply(new DOMMatrixReadOnly().scale(1.01, 1.01, 1.01, 0.0, 0.0, 0.0));
             
@@ -49,16 +61,20 @@ export default function transformation(element: HTMLElement): ReturnType<Action>
         }
     }
 
-    function onMouseUp(_: MouseEvent) {
+    function onUp(event: MouseEvent) {
         _initialPoint = null;
         _transformation = _current;
-        window.removeEventListener("mouseup", onMouseUp);   
+        window.removeEventListener("mouseup", onUp);   
+        element.removeEventListener("touchend", onUp);   
+        window.removeEventListener("mousemove", onMouseMove);   
+        element.removeEventListener("touchmove", onTouchMove);
         event.preventDefault();
+    
     }
 
     element.addEventListener("mousedown", onMouseDown);   
     element.addEventListener("touchstart", onTouchStart);
-    window.addEventListener("mousemove", onMouseMove);
+    
 
     return {
         update() {
